938cdc09af76ec47764c225c8138537bea76d842
Mr-Annonym <111135153+Mr-Annonym@users.noreply.github.com>
2024-10-01 19:13:18 +0200

    abstract symbol tree prototype

---
+++ src/utility/symbol_tree.c
@@ -0,0 +1,16 @@
+/** AUTHOR
+ * 
+ * @author <247581> Martin Mendl
+ * @file symbol_tree.c
+ * @date 28.9. 2024
+ * @brief Implementation of the symbol tree
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include "utility/symbol_tree.h"
+#include "utility/linked_list.h"
+#include "utility/enumerations.h"
+
